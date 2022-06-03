6b392385614709701f801dad5d17d22b5f17adf5
Mukul Agrawal <mukul2310@github.com>
2022-06-04 00:59:06 +0530

    made 7 pages

---
+++ constant.js
@@ -0,0 +1,39 @@
+let page1;
+page1 = {
+    title: 'Work Experience',
+    content:'lorem ipsum'
+}
+
+let page2;
+page1 = {
+    title: 'Work Experience',
+    content:'lorem ipsum'
+}
+
+
+let page3;
+page1 = {
+    title: 'Work Experience',
+    content:'lorem ipsum'
+}
+
+
+let page4;
+page1 = {
+    title: 'Work Experience',
+    content:'lorem ipsum'
+}
+
+
+let page5;
+page1 = {
+    title: 'Work Experience',
+    content:'lorem ipsum'
+}
+
+
+let page6;
+page1 = {
+    title: 'Work Experience',
+    content:'lorem ipsum'
+}
